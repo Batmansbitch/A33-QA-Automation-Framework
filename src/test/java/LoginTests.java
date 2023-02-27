f11a8d147149a3aad970f4ee0b8525c5b365c2a2
SHANNA HOWELL <124950266+Batmansbitch@users.noreply.github.com>
2023-02-26 22:29:48 -0600

    Update LoginTests.java
    Homework16

--- src/test/java/LoginTests.java
+++ src/test/java/LoginTests.java
@@ -6,18 +6,43 @@ import org.testng.Assert;
 import org.testng.annotations.Test;
 
 import java.time.Duration;
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.safari.SafariDriver;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.BeforeSuite;
 
-public class LoginTests extends BaseTest {
 
-    @Test
-    public static void LoginEmptyEmailPasswordTest() {
+public class BaseTest {
 
-        WebDriver driver = new ChromeDriver();
-        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+    @BeforeSuite
+    static void setupClass() {
+        WebDriverManager.chromedriver().setup();
+    }
 
-        String url = "https://apps.testpro.io/";
-        driver.get(url);
-        Assert.assertEquals(driver.getCurrentUrl(), url);
-        driver.quit();
+    public class LoginTests extends BaseTest {
+
+        @Test
+        public static void SuccessfulRegistrationTest() {
+
+            WebDriver driver = new ChromeDriver();
+            driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+
+            String url = "https://bbb.testpro.io/";
+            String urlRegistration = "https://bbb.testpro.io/registration.php"
+            driver.get(url);
+
+            WebElement registration = driver.findElement(By.id("Registration"));
+            registration.click();
+
+
+            Assert.assertEquals(driver.getCurrentUrl(), url);
+
+            Assert.assertEquals(driver.getCurrentUrl(), urlRegistration);
+            driver.quit();
+        }
     }
 }
